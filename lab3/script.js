6653b76b23bcfc9cb19cef8a03ce332bd36964db
sabina004 <sabinabazarkulova004@mail.ru>
2023-02-14 21:04:41 +0300

    adding imgs

---
+++ lab3/script.js
@@ -0,0 +1,21 @@
+alert("Привет, зайка♥")
+// const music=['weekend','mdee','Rihanna'];
+// function showArr(arr,elem){
+//    let out='' ;
+//    for(let i=0;i<arr.length;i++){
+// out+='<div><snd src="mp3\\${arr[i]}.mp3"><span>${i}</span></div>'
+//    }
+// document.querySelector(elem).innerHTML=out;
+// }
+// showArr(music, '.out-1-source-image');
+
+// const books=['weekend','mdee','Rihanna']
+// //     [
+
+
+// //    {title:'Недавно прочтенные книги',
+// //    subtitle:'Рекомендую к прочтению',
+// //    content:['Маленькие женщины','Хорошие жены',]
+// // }
+// // ];
+// document.querySelector('.books').innerHTML=`<table class="phone"><\phone>`;
